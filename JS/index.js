fac8822ea535aaa65214bc732c8987ed5800349a
Jul Guga <mr.guga89@hotmail.com>
2021-03-21 20:01:15 +0100

    Add back to top button, Various optimizations for gallery and images.

--- JS/index.js
+++ JS/index.js
@@ -1,29 +1,61 @@
 /*****************************************************************************/
+/****************************** Back to Top Button ***************************/
+/*****************************************************************************/
+
+//Get the button
+const mybutton = document.getElementById("myBtn");
+
+const mediaQueryScrollButton = window.matchMedia("(min-width: 769px)");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  // Check if the media query is true
+  if (mediaQueryScrollButton.matches) {
+    // Then trigger an alert
+    scrollFunction();
+  }
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+/*****************************************************************************/
 /******************************* SPLASH SCREEN *******************************/
 /*****************************************************************************/
 
-const sliderContainer = document.querySelector('.slide-container'),
-  slideRight = document.querySelector('.right-slide'),
-  slideLeft = document.querySelector('.left-slide'),
-  upButton = document.querySelector('.up-button'),
-  downButton = document.querySelector('.down-button'),
-  slidesLength = slideRight.querySelectorAll('div').length;
+const sliderContainer = document.querySelector(".slide-container"),
+  slideRight = document.querySelector(".right-slide"),
+  slideLeft = document.querySelector(".left-slide"),
+  upButton = document.querySelector(".up-button"),
+  downButton = document.querySelector(".down-button"),
+  slidesLength = slideRight.querySelectorAll("div").length;
 
 let activeSlideIndex = 0;
 
 slideLeft.style.top = `-${(slidesLength - 1) * 90}vh`;
 
-upButton.addEventListener('click', () => changeSlide('up'));
-downButton.addEventListener('click', () => changeSlide('down'));
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
 
 const changeSlide = (direction) => {
   // const sliderHeight = sliderContainer.clientHeight;
-  if (direction === 'up') {
+  if (direction === "up") {
     activeSlideIndex++;
     if (activeSlideIndex > slidesLength - 1) {
       activeSlideIndex = 0;
     }
-  } else if (direction === 'down') {
+  } else if (direction === "down") {
     activeSlideIndex--;
     if (activeSlideIndex < 0) {
       activeSlideIndex = slidesLength - 1;
@@ -53,14 +85,20 @@ const secondaryCarousel = new bootstrap.Carousel(carouselThumbs, {
   interval: false
 });
 
+$("#myCarousel").on("slide.bs.carousel", function(e) {
+  var id = parseInt($(e.relatedTarget).attr("data-bs-slide-number"));
+  $("[id^=carousel-selector-]").removeClass("selected");
+  $("[id=carousel-selector-" + id + "]").addClass("selected");
+});
+
 /*****************************************************************************/
 /********************************** F.A.Q ************************************/
 /*****************************************************************************/
 
-const toggles = document.querySelectorAll('.faq-toggle');
+const toggles = document.querySelectorAll(".faq-toggle");
 
 toggles.forEach((toggle) => {
-  toggle.addEventListener('click', () => {
-    toggle.parentNode.classList.toggle('active');
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
   });
 });
