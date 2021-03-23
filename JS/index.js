87d82ce32177730f5451194c944925c01cfda7d2
Jul Guga <mr.guga89@hotmail.com>
2021-03-23 16:14:49 +0100

    Change Splash Screen, Gallery to Cards, update new photos

--- JS/index.js
+++ JS/index.js
@@ -31,67 +31,6 @@ function topFunction() {
 }
 
 /*****************************************************************************/
-/******************************* SPLASH SCREEN *******************************/
-/*****************************************************************************/
-
-const sliderContainer = document.querySelector(".slide-container"),
-  slideRight = document.querySelector(".right-slide"),
-  slideLeft = document.querySelector(".left-slide"),
-  upButton = document.querySelector(".up-button"),
-  downButton = document.querySelector(".down-button"),
-  slidesLength = slideRight.querySelectorAll("div").length;
-
-let activeSlideIndex = 0;
-
-slideLeft.style.top = `-${(slidesLength - 1) * 90}vh`;
-
-upButton.addEventListener("click", () => changeSlide("up"));
-downButton.addEventListener("click", () => changeSlide("down"));
-
-const changeSlide = (direction) => {
-  // const sliderHeight = sliderContainer.clientHeight;
-  if (direction === "up") {
-    activeSlideIndex++;
-    if (activeSlideIndex > slidesLength - 1) {
-      activeSlideIndex = 0;
-    }
-  } else if (direction === "down") {
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesLength - 1;
-    }
-  }
-
-  // slideRight.style.transform = `translateY(-${
-  //   activeSlideIndex * sliderHeight
-  // }px)`;
-  // slideLeft.style.transform = `translateY(${
-  //   activeSlideIndex * sliderHeight
-  // }px)`;
-  slideRight.style.transform = `translateY(-${activeSlideIndex * 90}vh)`;
-  slideLeft.style.transform = `translateY(${activeSlideIndex * 90}vh)`;
-};
-
-/*****************************************************************************/
-/******************************** Gallery ************************************/
-/*****************************************************************************/
-
-const myCarousel = document.querySelector("#myCarousel")
-const primaryCarousel = new bootstrap.Carousel(myCarousel, {
-  interval: false
-});
-const carouselThumbs = document.querySelector("#carousel-thumbs")
-const secondaryCarousel = new bootstrap.Carousel(carouselThumbs, {
-  interval: false
-});
-
-$("#myCarousel").on("slide.bs.carousel", function(e) {
-  var id = parseInt($(e.relatedTarget).attr("data-bs-slide-number"));
-  $("[id^=carousel-selector-]").removeClass("selected");
-  $("[id=carousel-selector-" + id + "]").addClass("selected");
-});
-
-/*****************************************************************************/
 /********************************** F.A.Q ************************************/
 /*****************************************************************************/
 
